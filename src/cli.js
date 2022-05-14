ab88c2cb6db4d7895996cdb1870b7df0fc98c292
mini_mariya <m3305@mail.ru>
2022-05-14 17:10:48 +0300

    update .js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const askAboutName = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const UserName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ${UserName}!');
 };
 export default askAboutName;
