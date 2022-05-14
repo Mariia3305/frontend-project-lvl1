a5473326fae679b525dd90017cbd60fe3dc48207
mini_mariya <m3305@mail.ru>
2022-05-14 23:01:49 +0300

    add games

---
+++ src/games/game-even.js
@@ -0,0 +1,16 @@
+import engineOfGame from '../index.js';
+import generateRandomNumber from '../generateRandomNumber.js';
+
+const isEven = (number) => number % 2 === 0;
+
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const gameCheck = () => {
+    const number = generateRandomNumber(1, 500);
+    const question = `${number}`;
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    return [question, correctAnswer];
+  };
+const evenGame = () => engineOfGame(gameRules, gameCheck);
+
+export default evenGame;
