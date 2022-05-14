fec58d23f077d7f83fdfb973166dea51d0ee4a61
mini_mariya <m3305@mail.ru>
2022-05-14 23:00:40 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const roundCount = 3;
+
+const engineOfGame = (gameRules, gameCheck) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+  console.log(gameRules);
+
+  for (let i = 0; i < roundCount; i += 1) {
+    const [getQuestion, correctAnswer] = gameCheck();
+    console.log(`Question: ${getQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default engineOfGame;
