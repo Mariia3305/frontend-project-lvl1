71b404244d972122d19099197761e60187765e3b
mini_mariya <m3305@mail.ru>
2022-05-15 16:59:24 +0300

    add src/games/game-calc.js

---
+++ src/games/game-calc.js
@@ -0,0 +1,27 @@
+import engineOfGame from '../index.js';
+import generateRandomNumber from '../generateRandomNumber.js';
+
+const gameRules = 'What is the result of the expression?';
+
+const operator = ['+', '-', '*'];
+
+const calc = (number1, operator, number2) => {
+    switch (operator) {
+        case '+': return number1 + number2;
+        case '-': return number1 - number2;
+        case '*': return number1 * number2;
+        default: return null;
+    }
+};
+
+const gameCheck = () => {
+    const number1 = generateRandomNumber(1, 50);
+    const number2 = generateRandomNumber(1, 50);
+    const randomOperator = operator[generateRandomNumber(0, 2)]
+    const question = `${number1} ${randomOperator} ${number2}`;
+    const correctAnswer = `${calc(number1, randomOperator, number2)}`;
+    return [question, correctAnswer];
+  };
+const calcGame = () => engineOfGame(gameRules, gameCheck);
+
+export default calcGame;
