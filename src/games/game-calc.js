755f759610fbded2bf07eeb128f3f732bc1b4662
mini_mariya <m3305@mail.ru>
2022-05-21 17:23:56 +0300

    delete magic number

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -21,7 +21,7 @@ const calc = (number1, operator, number2) => {
 const runCheck = () => {
   const number1 = generateRandomNumber(1, 50);
   const number2 = generateRandomNumber(1, 50);
-  const randomOperator = operators[generateRandomNumber(0, 2)];
+  const randomOperator = operators[generateRandomNumber(0, operators.length)];
   const question = `${number1} ${randomOperator} ${number2}`;
   const correctAnswer = `${calc(number1, randomOperator, number2)}`;
   return [question, correctAnswer];
