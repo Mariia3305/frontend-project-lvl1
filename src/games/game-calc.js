1cc8386345419323a9d1ccd8221a0f0f7d496030
mini_mariya <m3305@mail.ru>
2022-05-21 19:08:22 +0300

    update

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -5,25 +5,21 @@ const rule = 'What is the result of the expression?';
 
 const operators = ['+', '-', '*'];
 
-const calc = (number1, operator, number2) => {
+const calculator = (number1, operator, number2) => {
   switch (operator) {
-    case '+': 
-     return number1 + number2;
-    case '-': 
-     return number1 - number2;
-    case '*': 
-     return number1 * number2;
-    default: 
-     return null;
+    case '+': return number1 + number2;
+    case '-': return number1 - number2;
+    case '*': return number1 * number2;
+    default: throw new Error(`Unknown operator: '${operator}'!`);
   }
 };
 
 const runCheck = () => {
   const number1 = generateRandomNumber(1, 50);
   const number2 = generateRandomNumber(1, 50);
-  const randomOperator = operators[generateRandomNumber(0, 2)];
+  const randomOperator = operators[generateRandomNumber(0, calculator.length)];
   const question = `${number1} ${randomOperator} ${number2}`;
-  const correctAnswer = `${calc(number1, randomOperator, number2)}`;
+  const correctAnswer = `${calculator(number1, randomOperator, number2)}`;
   return [question, correctAnswer];
 };
 const runCalcGame = () => runEngine(rule, runCheck);
